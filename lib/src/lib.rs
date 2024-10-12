fdde55a805619a581d700976ba69113f4b6e3900
Ayman <aymanshaik1015@gmail.com>
2024-10-12 09:02:28 +0000

    feat: p256 sig verify

--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -1,4 +1,6 @@
 use alloy_sol_types::sol;
+use p256::ecdsa::{signature::Verifier, Signature, VerifyingKey};
+use sha2::{Digest, Sha256};
 
 sol! {
     /// The public values encoded as a struct that can be easily deserialized inside Solidity.
@@ -19,4 +21,40 @@ pub fn fibonacci(n: u32) -> (u32, u32) {
         b = c;
     }
     (a, b)
+}
+
+pub fn sha256_hash(bytes: &[u8]) -> [u8; 32] {
+    let mut hasher = Sha256::new();
+    hasher.update(bytes);
+    hasher.finalize().into()
+}
+
+pub fn verify_ecdsa_p256_signature(
+    message: &[u8],
+    signature_hex: &str,
+    public_key_hex: &str,
+) -> Result<bool, Box<dyn std::error::Error>> {
+    let signature_bytes = hex::decode(signature_hex)?;
+    let signature = Signature::from_slice(&signature_bytes)?;
+
+    let public_key_bytes = hex::decode(public_key_hex)?;
+    let verifying_key = VerifyingKey::from_sec1_bytes(&public_key_bytes)?;
+
+    Ok(verifying_key.verify(message, &signature).is_ok())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_verify_ecdsa_p256_signature() {
+        let message =
+            b"ECDSA proves knowledge of a secret number in the context of a single message";
+        let public_key_hex = "0457be97dd389c893d7271a1fe7546aaf09074aba40779d19c21c00832bc3f821add286faf7beb2f0722050169d89ae7fe0b02e8b8bea4c5141b188ff678e6d8bf";
+        let signature_hex = "6a7570a91dd49c4ff738efd81ceaadbf89daad02611d184e276906eeb36712254ff40cda556ed67ef04b3933e2e92830b6cfae684da605f07f779fad78945e22";
+
+        let result = verify_ecdsa_p256_signature(message, signature_hex, public_key_hex);
+        assert!(result.unwrap());
+    }
 }
--- program/src/main.rs
+++ program/src/main.rs
@@ -9,16 +9,24 @@
 sp1_zkvm::entrypoint!(main);
 
 use alloy_sol_types::SolType;
-use fibonacci_lib::{fibonacci, PublicValuesStruct};
+use fibonacci_lib::{fibonacci, sha256_hash, verify_ecdsa_p256_signature, PublicValuesStruct};
 
 pub fn main() {
     // Read an input to the program.
     //
     // Behind the scenes, this compiles down to a custom system call which handles reading inputs
     // from the prover.
+
     let n = sp1_zkvm::io::read::<u32>();
 
-    // Compute the n'th fibonacci number using a function from the workspace lib crate.
+    let sha256_hash = sha256_hash(&[1, 11]);
+
+    let message = b"ECDSA proves knowledge of a secret number in the context of a single message";
+    let public_key_hex = "0457be97dd389c893d7271a1fe7546aaf09074aba40779d19c21c00832bc3f821add286faf7beb2f0722050169d89ae7fe0b02e8b8bea4c5141b188ff678e6d8bf";
+    let signature_hex = "6a7570a91dd49c4ff738efd81ceaadbf89daad02611d184e276906eeb36712254ff40cda556ed67ef04b3933e2e92830b6cfae684da605f07f779fad78945e22";
+
+    let result = verify_ecdsa_p256_signature(message, signature_hex, public_key_hex);
+
     let (a, b) = fibonacci(n);
 
     // Encode the public values of the program.
